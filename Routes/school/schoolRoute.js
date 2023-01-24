dbcba7e614e870179a4b54a5c2b6b081ec1cc3e6
Taraka <tharakaariyarathna04@gmail.com>
2023-01-25 02:20:13 +0530

    Completed

--- Routes/school/schoolRoute.js
+++ Routes/school/schoolRoute.js
@@ -1,9 +1,11 @@
 import express from 'express' ;
+import axios from 'axios' ;
 
 const router = express.Router() ;
 const schoolData = [] ;
 const studentData = [] ;
 let numberOfSchools = 0 ;
+const addStudentsEvent = "addStudent" ;
 
 router.post("/", (req, res) => {
     const {name, eventName, eventUrl} = req.body ;
@@ -16,17 +18,26 @@ router.post("/", (req, res) => {
         }
     }
     schoolData.push(school) ;
+    numberOfSchools = numberOfSchools + 1 ;
 
     return res.status(200).json({message:"Student added", school}) ;
 })
 
-router.post("/addStudent", (req, res) => {
-    const {name, schoolId, eventName} = req.body ;
-    const school = schoolData.filter(school => school.id != schoolId) 
+router.post("/addStudent", async(req, res) => {
+    const {name, schoolId} = req.body ;
+    const school = schoolData[0] ;
     const student = {
         name : name,
         school : school.name
     }
+    if(school.webhookDetails.eventName == 'addStudent'){
+        const response = await axios.post(school.webhookDetails.eventUrl, {studentName : student.name}) ;
+        console.log(response.data) ;
+        return res.status(200).json(response.data) ;
+    }
+
+    return res.status(400).json("error") ;
+
 })
 
 export default router ;
