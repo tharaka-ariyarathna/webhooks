d5a0d76cfdb03726387eaab7d02fd7c3b646bcf5
Taraka <tharakaariyarathna04@gmail.com>
2023-01-25 01:42:52 +0530

    created api for add student

--- Routes/school/schoolRoute.js
+++ Routes/school/schoolRoute.js
@@ -1,7 +1,32 @@
 import express from 'express' ;
 
 const router = express.Router() ;
-
+const schoolData = [] ;
 const studentData = [] ;
+let numberOfSchools = 0 ;
+
+router.post("/", (req, res) => {
+    const {name, eventName, eventUrl} = req.body ;
+    const school = {
+        name : name,
+        id : numberOfSchools + 1,
+        webhookDetails: {
+            eventName : eventName,
+            eventUrl : eventUrl
+        }
+    }
+    schoolData.push(school) ;
+
+    return res.status(200).json({message:"Student added", school}) ;
+})
+
+router.post("/addStudent", (req, res) => {
+    const {name, schoolId, eventName} = req.body ;
+    const school = schoolData.filter(school => school.id != schoolId) 
+    const student = {
+        name : name,
+        school : school.name
+    }
+})
 
 export default router ;
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express' ;
 import testRoute from './Routes/testRoute.js' ;
+import schoolRoute from './Routes/school/schoolRoute.js' ;
+import bodyParser from 'body-parser';
 
 const App = express() ;
+App.use(bodyParser.json({ limit: "30mb", extended: true }));
+App.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 App.listen(5000, () => {
     console.log("Listening") ;
 })
 
 App.use("/test", testRoute) ;
+App.use("/school", schoolRoute) ;
